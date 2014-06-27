243cfe21a5dbc40629b84da76a2cdec5d27115e3
shrikanthak <shrikanth_ak@yahoo.com>
2014-06-27 16:41:13 -0400

    First P1 commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<head>
+    <title>
+       My first online web page
+    </title>
+
+    <style>
+        .h1Heading
+        {
+        }
+        .p1Formatting
+        {
+        }
+
+    </style>
+
+</head>
+
+<body>
+
+    <div>
+
+        <img src="./images/ShriAnan.jpg" style="display:block;margin-left:auto;margin-right:auto;"/>
+
+    </div>
+
+    <div>
+        <ul>
+            <li>
+                <h3>
+                    In 2+ sentences describe who you are and what you do
+                </h3>
+                <p>
+                    My name is Shrikanth Ananthakrishnan. I work as a software developer for Dassault Systemes in their Lifecycle Management Group.
+                </p>
+            </li>
+            <li>
+                <h3>Why are you taking this course?</h3>
+                <p>I want to learn how to program web applications. This will be an important skill set to add to my resume</p>
+            </li>
+            <li>
+                <h3>What previous experience do you have with HTML/CSS?</h3>
+                <p>
+                    I have a little experience working with HTML. I have created and deployed a couple of small application
+                    but all through Visual Studio using ASP.Net. I understand the concepts of HTML and CSS, but this will be the first time I'll develop something from scratch.
+                </p>
+            </li>
+            <li>
+                <h3>What previous experience do you have with programming?</h3>
+                <p>Enough to be dangerous</p>
+            </li>
+            <li>
+                <h3>Have you worked with PHP before? If so, to what extent?</h3>
+                <p>I have no experience with PHP</p>
+            </li>
+            <li>
+                <h3>Describe your level of experience with programming.</h3>
+                <p>I have a total of around 9 years in programming primarily Windows applications using different languages</p>
+            </li>
+            <li>
+                <h3>Are you working on a Mac, PC, or some other platform?</h3>
+                <p>PC</p>
+            </li>
+            <li>
+                <h3>Anything else we should know?</h3>
+                <p>I have nothing extraordinary to mention</p>
+            </li>
+        </ul>
+    </div>
+
+<h1>
+
+</h1>
+
+</body>
